4468f3d27cf0efd575bb6df64563286a5274e9db
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2022-09-12 14:43:46 +0200

    add

---
+++ js/naming.js
@@ -0,0 +1,21 @@
+// var 1 pet_preferito = 'gatto';
+// console.log(1 pet_preferito);
+// in questo caso var 1 pet_preferito = 'gatto' non va bene, la console reinderizza come Uncaught error in quanto stiamo utilizzando un number (1)
+
+var pet = 'gatto';
+console.log(pet);
+var Pet = 'cane';
+console.log(Pet);
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
